ad5f59ee443ccf00ee91e9d04972fa6a19e81dd5
mitul078 <mituljodhani788@gmail.com>
2025-06-24 09:58:31 +0530

    api

--- frontend/api/axiosConfig.jsx
+++ frontend/api/axiosConfig.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: "https://backendserver-yakq.onrender.com/"
-    baseURL:"http://localhost:3000"
+    baseURL: "https://backendserver-yakq.onrender.com/",
 })
 
 export default instance
